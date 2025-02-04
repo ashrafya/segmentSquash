f50caf903615b81e9b05d447b09468db2a293665
Yawar Ashraf <yawar1ashraf@gmail.com>
2025-02-04 16:16:16 -0500

    pics to analyze for players and court lines

--- src/video.py
+++ src/video.py
@@ -1,4 +1,5 @@
 import cv2
+import random
 
 
 class matchVideo:
@@ -33,8 +34,36 @@ class matchVideo:
     return length
 
 
+  def download_images(self, frame_num: int) -> None:
+    """picks images and downloads them into data directory"""
+    cap = cv2.VideoCapture(self.filepath)
+    count = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        if count == frame_num:
+          cv2.imwrite("../data/frame-%d.jpg" % count, frame)
+          break
+
+        # frame = cv2.resize(frame, (1280, 720))  # Resize for efficiency
+        # cv2.imshow('Frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):   # break command
+            break
+        count += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == "__main__":
   filepath = "../data/Farag v Coll Tournament of Champions 2025  SF HIGHLIGHTS.mp4"
   match = matchVideo(filepath)
-  match.play_video()
-  print(match.count_frames())
+  # match.play_video()
+  # print(match.count_frames())
+  for i in range(9):
+    random_int = random.randint(40, 9000)
+    match.download_images(random_int)
+    
